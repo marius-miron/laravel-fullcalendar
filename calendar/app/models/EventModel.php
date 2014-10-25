c6d67ad5329e5b554678d5430342e944a8ff2cda
Marius Miron <marius.costel.miron@gmail.com>
2014-10-25 19:24:12 +0300

    creating event model

---
+++ calendar/app/models/EventModel.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of Event
+ *
+ * @author marius
+ */
+class EventModel extends Eloquent {
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'events';
+    
+}
