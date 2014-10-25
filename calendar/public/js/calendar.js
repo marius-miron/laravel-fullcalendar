63ced95316bbbe0d25d70859632e4f37a6b08c11
Marius Miron <marius.costel.miron@gmail.com>
2014-10-25 19:16:21 +0300

    adding edit functionality adding mini_calendar left column

--- calendar/public/js/calendar.js
+++ calendar/public/js/calendar.js
@@ -6,69 +6,65 @@
 
 $(document).ready(function () {
 
-                // page is now ready, initialize the calendar...
+    // page is now ready, initialize the calendar...
 
-                $('#calendar').fullCalendar({
-                    header: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'month,agendaWeek,agendaDay'
-                    },
-                    select: function(start, end) {
-				var title = prompt('Event Title:');
-				var eventData;
-				if (title) {
-					eventData = {
-						title: title,
-						start: start,
-						end: end
-					};
-					$('#calendar').fullCalendar('renderEvent', eventData, true);
-                                        $.post('asdads', eventData.title);
-				}
-				$('#calendar').fullCalendar('unselect');
-			},
-                    selectable: true,
-                    editable: true,
-                    defaultView: 'month',
-                    events: [
-                        {
-                            title: 'All Day Event',
-                            start: '2014-06-01'
-                        },
-                        {
-                            title: 'Long Event',
-                            start: '2014-06-07',
-                            end: '2014-06-10'
-                        },
-                        {
-                            id: 999,
-                            title: 'Repeating Event',
-                            start: '2014-06-09T16:00:00'
-                        },
-                        {
-                            id: 999,
-                            title: 'Repeating Event',
-                            start: '2014-06-16T16:00:00'
-                        },
-                        {
-                            title: 'Meeting',
-                            start: '2014-06-12T10:30:00',
-                            end: '2014-06-12T12:30:00'
-                        },
-                        {
-                            title: 'Lunch',
-                            start: '2014-06-12T12:00:00'
-                        },
-                        {
-                            title: 'Birthday Party',
-                            start: '2014-06-13T07:00:00'
-                        },
-                        {
-                            title: 'Click for Google',
-                            url: 'http://google.com/',
-                            start: '2014-06-28'
-                        }
-                    ]
-                });
-            });
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        select: function (start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#calendar').fullCalendar('renderEvent', eventData, true);
+                $.post('calendar', {'date': start.format(), 'text': title});
+            }
+            $('#calendar').fullCalendar('unselect');
+        },
+        selectable: true,
+        editable: true,
+        defaultView: 'month',
+        eventLimit: true,
+        events: {url: 'calendar'},
+        eventClick: function (event) {
+            var title = prompt('Edit:');
+            event.title = title;
+            $('#calendar').fullCalendar('updateEvent', event);
+            $.post('calendar/edit', {'id' : event.id, 'text': title});
+        }
+    });
+
+
+    $('#mini_calendar').fullCalendar({
+        header: {
+            left: 'prev,next',
+            center: 'title'
+        },
+        select: function (start, end) {
+            var title = prompt('Event Title:');
+            var eventData;
+            if (title) {
+                eventData = {
+                    title: title,
+                    start: start,
+                    end: end
+                };
+                $('#mini_calendar').fullCalendar('renderEvent', eventData, true);
+                $.post('calendar', {'date': start.format(), 'text': title});
+            }
+            $('#mini_calendar').fullCalendar('unselect');
+        },
+        selectable: true,
+        editable: true,
+        defaultView: 'month',
+        eventLimit: true,
+        events: {url: 'calendar'}
+    });
+});
